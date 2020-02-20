bc841e245c198bdbbd2a855f36bfc6ea41b68acf
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:00:28 +0530

    Initial commit

---
+++ delegate2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate2
+{
+    class Program
+    {
+        public delegate string Mydel(int x);
+
+        static void Main(string[] args)
+        {
+            Mydel delg = new Mydel(Area);
+            Console.WriteLine(delg(5));
+            delg += new Mydel(Area);
+
+        }
+        public static string Area(int a)
+        {
+            int c = a * a;
+            return Convert.ToString(c);
+        }
+    }
+}
